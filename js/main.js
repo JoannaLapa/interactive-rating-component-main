f61f4c3d08730c8ac9168df8060c1d2496afccc0
Joanna <Bartek@MacBook-Pro-Bartosz.local>
2022-04-24 19:29:05 +0200

    classes downloaded to js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const rateContainer = document.querySelector('.rating-state__container')
+const thankyouContainer = document.querySelector('.thankyou-state__container')
+const circle = document.querySelectorAll('.rating-state__circle')
+const btn = document.querySelectorAll('.rating-state__btn')
+const score = document.querySelectorAll('.thankyou-state__score')
+
+const greyBgc = () => {}

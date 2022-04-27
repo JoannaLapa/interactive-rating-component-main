5e8703d1f1a42e01ecb66b71f341a41b47dba074
Joanna <Bartek@MacBook-Pro-Bartosz.local>
2022-04-27 23:04:28 +0200

    javascript functions added - changing circle color for click, changing rate-container for thankyou-container, adding final score, deactivate class to html added and styled

--- js/main.js
+++ js/main.js
@@ -1,12 +1,31 @@
 const rateContainer = document.querySelector('.rating-state__container')
 const thankyouContainer = document.querySelector('.thankyou-state__container')
 const circles = document.querySelectorAll('.rating-state__circle')
-const btn = document.querySelectorAll('.rating-state__btn')
-const score = document.querySelectorAll('.thankyou-state__score')
+const btn = document.querySelector('.rating-state__btn')
+const score = document.querySelector('.thankyou-state__score')
+const previousElement = document.querySelector('.rating-state__circle-grey')
+const deactivate = document.querySelector('.deactivate')
 
-const greyBgc = () => {
-    
-// for (let i = 0; i< circles.length; i++){
-//     if circles[i].addEventListener 
+const removeGreyBgc = () => {
+	circles.forEach(circle => circle.classList.remove('rating-state__circle-grey'))
 }
 
+const changeWindows = () => {
+	if(score.innerHTML != ''){
+	rateContainer.classList.add('deactivate')
+	thankyouContainer.classList.remove('deactivate')}
+}
+
+const addGreyBgc = () => {
+	for (let circle of circles) {
+		circle.addEventListener('click', function () {
+			removeGreyBgc()
+			circle.classList.add('rating-state__circle-grey')
+			score.textContent = circle.innerHTML
+		})
+	}
+}
+
+addGreyBgc()
+
+btn.addEventListener('click', changeWindows)
